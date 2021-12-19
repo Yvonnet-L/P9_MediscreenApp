ee79b992fae5febb2bfc030923ca03bd0f1fcb29
Laurent Yvonnet <yvonnet_l@yahoo.fr>
2021-12-19 16:35:00 +0100

    commit before push

---
+++ assessment-microservice/src/main/java/com/mediscreen/assessmentmicroservice/AssessmentMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.assessmentmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentMicroserviceApplication.class, args);
+	}
+
+}
---
+++ assessment-microservice/src/test/java/com/mediscreen/assessmentmicroservice/AssessmentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.assessmentmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
