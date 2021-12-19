88f95f541c336be5e234c37db3bda899b52be180
Laurent Yvonnet <yvonnet_l@yahoo.fr>
2021-12-19 15:57:51 +0100

    commit before merge

--- clientui/src/main/java/com/clientui/clientui/service/IHistoryService.java
+++ clientui/src/main/java/com/clientui/clientui/service/IHistoryService.java
@@ -4,24 +4,8 @@ import com.clientui.clientui.dto.PatientHistoryDTO;
 
 import java.util.List;
 
-public interface IHistoryService {
-    /**
-     *     @GetMapping("/patHistories/{patientId}")
-     *     List<PatientHistoryDTO> getPatientHistoryBypatientId(@PathVariable(name = "patientId") Integer patientId);
-     *
-     *     @PostMapping("/patHistory/add")
-     *     PatientHistoryDTO addPatientHistory(@Valid @RequestBody PatientHistoryDTO patientHistoryDTO);
-     *
-     *     @DeleteMapping(value="/patHistory/delete/{id}")
-     *     void deletePatientHistory(@PathVariable("id") String id);
-     *
-     *     @GetMapping("/patHistory/{id}")
-     *     PatientHistoryDTO getPatientHistoryById(@PathVariable(name = "id") String id);
-     *
-     *     @PutMapping("/patHistory/update/{id}")
-     *     PatientHistoryDTO updatePatientHistory(@PathVariable("id") String id,@Valid @RequestBody PatientHistoryDTO patientHistoryDTO);
-     */
 
+public interface IHistoryService {
 
     List<PatientHistoryDTO> findAllPatientHistories();
 
